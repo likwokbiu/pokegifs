2e8baf2963a9a11434bd95638da29ecf30bbd509
likwokbiu <li_bill@ymail.com>
2018-03-19 14:45:04 -0400

    adding pokemon info

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,6 +5,11 @@ class PokemonsController < ApplicationController
   end
 
   def show
-    render json: { "message": "ok" }
+    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(res.body)
+    name = body["name"]
+    id = body["id"]
+    type = body["types"][0]["type"]["name"]
+    render json: { "id": id, "name": name, "types": [type] }
   end
 end
